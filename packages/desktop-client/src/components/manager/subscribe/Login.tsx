289a372068a7790082e7ca29915945c86a4ab8be
Zach Whelchel <zach.whelchel@gmail.com>
2025-02-10 16:23:48 -0500

    WIP

--- packages/desktop-client/src/components/manager/subscribe/Login.tsx
+++ packages/desktop-client/src/components/manager/subscribe/Login.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
+import { Auth0Lock } from 'auth0-lock';
 
 import { isElectron } from 'loot-core/shared/environment';
 import { loggedIn } from 'loot-core/src/client/actions/user';
@@ -23,6 +24,7 @@ import { useAvailableLoginMethods, useLoginMethod } from '../../ServerContext';
 
 import { useBootstrapped, Title } from './common';
 import { OpenIdForm } from './OpenIdForm';
+import * as colorPalette from '../../../style/palette';
 
 function PasswordLogin({ setError, dispatch }) {
   const [password, setPassword] = useState('');
@@ -115,7 +117,7 @@ function OpenIdLogin({ setError }) {
     <View>
       {!reviewOpenIdConfiguration && (
         <>
-          <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
             <Button
               variant="primary"
               style={{
@@ -126,7 +128,7 @@ function OpenIdLogin({ setError }) {
               }}
               onPress={onSubmitOpenId}
             >
-              <Trans>Sign in with OpenID</Trans>
+              <Trans>Sign In</Trans>
             </Button>
           </View>
           {warnMasterCreation && (
@@ -252,45 +254,7 @@ export function Login() {
   }
 
   return (
-    <View style={{ maxWidth: 450, marginTop: -30, color: theme.pageText }}>
-      <Title text={t('Sign in to this Actual instance')} />
-
-      {loginMethods?.length > 1 && (
-        <Text
-          style={{
-            fontSize: 16,
-            color: theme.pageTextDark,
-            lineHeight: 1.4,
-            marginBottom: 10,
-          }}
-        >
-          <Trans>
-            If you lost your password, you likely still have access to your
-            server to manually reset it.
-          </Trans>
-        </Text>
-      )}
-
-      {loginMethods?.length > 1 && (
-        <View style={{ marginTop: 10 }}>
-          <Label
-            style={{
-              ...styles.verySmallText,
-              color: theme.pageTextLight,
-              paddingTop: 5,
-            }}
-            title={t('Select the login method')}
-          />
-          <Select
-            value={method}
-            onChange={newValue => {
-              setError(null);
-              setMethod(newValue);
-            }}
-            options={loginMethods?.map(m => [m.method, m.displayName])}
-          />
-        </View>
-      )}
+    <View style={{ color: theme.pageText, marginTop: -43 }}>
 
       {error && (
         <Text
@@ -305,13 +269,7 @@ export function Login() {
         </Text>
       )}
 
-      {method === 'password' && (
-        <PasswordLogin setError={setError} dispatch={dispatch} />
-      )}
-
-      {method === 'openid' && <OpenIdLogin setError={setError} />}
-
-      {method === 'header' && <HeaderLogin error={error} />}
+      <OpenIdLogin setError={setError} />
     </View>
   );
 }
